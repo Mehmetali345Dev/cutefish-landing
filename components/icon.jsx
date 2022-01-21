04747838ab5798d1cc3d97f4f841dbcf25496401
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2022-01-21 18:24:59 +0300

    feat: add transitions

--- components/icon.jsx
+++ components/icon.jsx
@@ -2,8 +2,20 @@ import { GithubLogo, TwitterLogo } from "phosphor-react";
 export default function Icon(props) {
   return (
     <div>
-      {props.name === "github" && <GithubLogo size={20} weight="fill" />}
-      {props.name === "twitter" && <TwitterLogo size={20} weight="fill" />}
+      {props.name === "github" && (
+        <GithubLogo
+          size={20}
+          weight="fill"
+          className="transition-all ease-in-out duration-200 hover:text-blue-600"
+        />
+      )}
+      {props.name === "twitter" && (
+        <TwitterLogo
+          size={20}
+          weight="fill"
+          className="transition-all ease-in-out duration-200 hover:text-blue-600"
+        />
+      )}
     </div>
   );
 }

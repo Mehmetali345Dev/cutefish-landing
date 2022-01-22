6c53b998ec209d7714e453c75d75990377adb955
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2022-01-22 09:46:43 +0300

    refactor: add border radius

--- components/cards/apps.jsx
+++ components/cards/apps.jsx
@@ -1,6 +1,6 @@
 export default function Apps() {
   return (
-    <div className="md:flex grid justify-between w-full items-center px-8 md:px-32 py-8 bg-gradient-to-r from-indigo-300 to-pink-300">
+    <div className="md:flex md:rounded-none rounded-md grid justify-between w-full items-center px-8 md:px-32 py-8 bg-gradient-to-r from-indigo-300 to-pink-300">
       <div className="grid justify-items-start gap-3 flex-grow">
         <h1 className="text-3xl text-white font-bold">Awesome Apps</h1>
         <p className="md:w-4/6 text-lg text-white">

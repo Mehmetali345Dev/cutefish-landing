cdb6152c09a585c99a98921236f1f7abe2b00427
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2022-01-23 15:32:44 +0300

    refactor: add styled corners

--- components/cards/desktop.jsx
+++ components/cards/desktop.jsx
@@ -3,7 +3,7 @@ export default function Desktop() {
   const { t } = useTranslation();
 
   return (
-    <div className="md:flex grid justify-between w-full items-center px-8 md:px-32 py-8 bg-gradient-to-r from-green-300 to-indigo-300">
+    <div className="md:flex grid md:rounded-none rounded-md justify-between w-full items-center px-8 md:px-32 py-8 bg-gradient-to-r from-green-300 to-indigo-300">
       <img src="/desktop.png" className="rounded-md md:w-3/6 w-full " />
       <div className="grid justify-items-start md:justify-items-end md:text-right gap-3 flex-grow">
         <h1 className="text-3xl text-white font-bold">
